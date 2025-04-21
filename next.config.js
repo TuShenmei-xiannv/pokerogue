08cb777ebd0b11b8382f4b4fc28aa24ca4f66fdc
tushenmei <tushenmei@gmail.com>
2025-04-21 11:50:40 +0800

    fix: update cloudflare pages configuration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     dirs: ['.'],
   },
@@ -13,5 +14,11 @@ module.exports = withBundleAnalyzer({
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
-});
+  reactStrictMode: false,
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
