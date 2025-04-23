0080819ccda54d33c77356f042598e0d9b451665
tushenmei <tushenmei@gmail.com>
2025-04-23 20:17:37 +0800

    support GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,49 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // @ts-ignore
+      window.gtag('config', 'G-RT6T53G4YH', {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-RT6T53G4YH"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RT6T53G4YH');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
 
 export default MyApp;
